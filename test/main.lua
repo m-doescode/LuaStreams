b4f02f6cddcb0834313635e6909b83a8c516e017
m-doescode <80221594+m-doescode@users.noreply.github.com>
2022-04-17 22:56:30 +0200

    Added tests for each function

--- test/main.lua
+++ test/main.lua
@@ -1,15 +1,61 @@
 local Streams = require('../Streams')
 local Stream = Streams.Stream
 
-local printtable = require("printtable")
+function compareTables(t1, t2)
+    if t1 == t2 then return true end
+    if #t1 ~= #t2 then return false end
+    for k, v in pairs(t1) do
+        if t1[k] ~= t2[k] then
+            return false
+        end
+    end
+    return true
+end
 
-local oldt = {1, 2, 3, 4}
-local t = Stream.new(oldt)
+-- Map
+local original = {1, 2, 3, 4}
+local model = {2, 3, 4, 5}
+local transformed = Stream.new(original)
             :map(function(x) return x + 1 end)
             :collect()
 
-printtable(oldt)
-printtable(t)
+assert(compareTables(model, transformed), "Map test failed!")
 
+-- Filter
+local original = {1, 2, 3, 4}
+local model = {2, 4}
+local transformed = Stream.new(original)
+            :filter(function(x) return x % 2 == 0 end)
+            :collect()
+
+for k,v in pairs(transformed) do print(k, v) end
+assert(compareTables(model, transformed), "Filter test failed!")
+
+-- Sorted
+local original = {4, 2, 3, 1}
+local model = {1, 2, 3, 4}
+local transformed = Stream.new(original)
+            :sorted()
+            :collect()
+
+assert(compareTables(model, transformed), "Sorted test failed!")
+
+-- Skip
+local original = {1, 2, 3, 4}
+local model = {3, 4}
+local transformed = Stream.new(original)
+            :skip(2)
+            :collect()
+
+assert(compareTables(model, transformed), "Skip test failed!")
+
+-- Limit
+local original = {1, 2, 3, 4}
+local model = {1, 2}
+local transformed = Stream.new(original)
+            :limit(2)
+            :collect()
+
+assert(compareTables(model, transformed), "Limit test failed!")
 
 return {}
